1532843e362688ea5d2a61d8fa81f242f1c37c4e
Brianna <49692700+blijewski@users.noreply.github.com>
2019-10-25 21:46:23 -0600

    Updated BadConfigFormatException to include a better named variable

--- src/clueGame/BadConfigFormatException.java
+++ src/clueGame/BadConfigFormatException.java
@@ -1,12 +1,13 @@
 package clueGame;
 
+@SuppressWarnings("serial")
 public class BadConfigFormatException extends Exception{ //serializable class BadConfigFormatException does not declare a static final serialVersionUID field of type long
 
 	public BadConfigFormatException() {
 		super("Config Error Occured");
 	}
 	
-	public BadConfigFormatException(String m) {
-		super(m);
+	public BadConfigFormatException(String message) {
+		super(message);
 	}
 }
